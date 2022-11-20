8839742758daaf7135eb41a594e693a81f7f32ca
Ramirocco <105560507+Ramirocco@users.noreply.github.com>
2022-11-20 00:14:27 -0300

    agregue ItemListContainer, Primera Preentrega Rocco Ramiro

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { NavBar } from "./components/NavBar/NavBar";
 
 const App = ()=>{
     return (
         <div>
         <NavBar/>
+        <ItemListContainer/>
         </div>
     )
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,5 +1,23 @@
-export const ItemListContainer = (greeting)=> {
-    return ( 
-        <p>Mensaje</p>
+export const ItemListContainer = (greeting) => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href="">Gorras y antiparras</a>
+                </li>
+                <li>
+                    <a href="">Ropa</a>
+                </li>
+                <li>
+                    <a href="">Toallas y bolsos</a>
+                </li>
+                <li>
+                    <a href="">Relojes</a>
+                </li>
+                <li>
+                    <a href="">Accesorios deportivos</a>
+                </li>
+            </ul>
+        </div>
     );
 }
